084f2e224972148c333e0850141411f0e980b813
muhammad.arifin <arifin.muhammad2610@gmail.com>
2020-12-29 18:49:00 +0700

    rewrite event.key event handler to moveBox()

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ window.addEventListener('load', () => {
 	block.style.top = parseInt(Math.floor(Math.random() * (containerDimension - 50)))+'px';
 });
 
-window.addEventListener('keydown', (e) => {
-	switch(e.key) {
+function moveBox(event) {
+	switch(event.key) {
 		case 'ArrowLeft':
 			if(parseInt(block.style.left) >= 5) {
 				block.style.left = parseInt(block.style.left) - moveBy + 'px';
@@ -48,4 +48,8 @@ window.addEventListener('keydown', (e) => {
 	}
 	console.log("left: " + block.style.left);
 	console.log("top: " + block.style.top + "\n");
+}
+
+window.addEventListener('keydown', (event) => {
+	moveBox(event);
 });
